1301e96cbc69ac1bd52ceef3446b04f7658c90e8
Daniel Eck <Danny.C.eck@gmail.com>
2017-11-29 17:03:44 -0700

    Update whitelist-cloudflare-ips-cron.sh

--- whitelist-cloudflare-ips-cron.sh
+++ whitelist-cloudflare-ips-cron.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 echo "#Cloudflare" > /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf;
-        iptables -F cloudflare;
-        iptables -N cloudflare;
-for i in `curl https://www.cloudflare.com/ips-v4`; do
-        iptables -A cloudflare -p tcp --sport 2408 -s $i -j ACCEPT;
-        iptables -A cloudflare -p tcp -m multiport --dports http,https -s $i -j ACCEPT;
-        echo "set_real_ip_from $i;" >> /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf;
+iptables -F cloudflare;
+iptables -N cloudflare;
+for ip in `curl https://www.cloudflare.com/ips-v4`; do
+        iptables -A cloudflare -I INPUT -p tcp -m multiport --dports http,https -s "$ip" -j ACCEPT;
+        echo "set_real_ip_from $ip;" >> /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf;
 done
 
-iptables-save > /etc/sysconfig/iptables
+ip6tables -F cloudflare;
+ip6tables -N cloudflare;
+for ip in `curl https://www.cloudflare.com/ips-v6`; do
+        ip6tables -A cloudflare -I INPUT -p tcp -m multiport --dports http,https -s "$ip" -j ACCEPT;
+        echo "set_real_ip_from $ip;" >> /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf;
+done
+
 echo "real_ip_header CF-Connecting-IP;" >> /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf;
+
+#Debian/Ubuntu
+iptables-save > /etc/iptables/rules.v4
+ip6tables-save > /etc/iptables/rules.v6
+
+#RHEL/CentOS
+#iptables-save > /etc/sysconfig/iptables
+#ip6tables-save > /etc/sysconfig/ip6tables
